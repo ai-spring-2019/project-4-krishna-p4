205d3069ab88dbcf3b1da84e1851a551f4114950
krashnerd <krishkringle1@gmail.com>
2019-05-13 21:33:40 -0400

    Commenting

--- project4.py
+++ project4.py
@@ -43,6 +43,9 @@ def convert_data_to_pairs(data, header):
                 x.append(element)
         pair = (x, y)
         pairs.append(pair)
+
+    # If there needs to be more than 2 classes,
+    # converts numbers to arrays of zeros except for the corresponding element
     multiclass = False
     min_val = np.inf
     max_val = -np.inf
@@ -61,11 +64,7 @@ def convert_data_to_pairs(data, header):
             y_class = int(y[0] - min_val)
             pairs[i] = (x, [0] * num_classes)
             pairs[i][1][y_class] = 1
-
-    print(pairs[0])
     return pairs
-
-
 
 def dot_product(v1, v2):
     """Computes the dot product of v1 and v2"""
@@ -105,6 +104,7 @@ def accuracy(nn, pairs):
         if len(nn.output_layer) == 1:
             if class_prediction != y[0]:
                 true_positives += 1
+        # Modification to handle multi-class
         else:
             if y[class_prediction] != 1:
                 true_positives += 1
@@ -114,19 +114,32 @@ def accuracy(nn, pairs):
 
     return 1 - (true_positives / total)
 
+### Helpers
+def dynamic_alpha(count, const):
+    return const / (const + count)
+
+def static_alpha(count, const):
+    return const
+
+def k_groups(data, k):
+    """Divides the data into k approximately equal-sized groups"""
+    random.shuffle(data)
+    groups = []
+    for i in range(k):
+        groups.append(data[int(i*len(data)/k):int((i+1)*len(data)/k)])
+    return groups
 
 
-################################################################################
-### Neural Network code goes here
 
 class Layer():
+    """ Input or hidden layer for a neural network"""
     def __init__(self, size, next_size):
+        """Initializes a layer given the size of the layer and the size of the next layer"""
+        
+        # Create random (size+1) x next_size array of random numbers
         self.weights = np.random.rand(size + 1, next_size)
         self.activations = []
         self.deltas = []
-        self.prediction_info = None
-        # self.nodes = [Node() for _ in range(self.size)]
-        # self.weights = [node.weights for node in self.nodes]
 
     def __len__(self):
         return len(self.weights)
@@ -135,31 +148,35 @@ class Layer():
         self.next_layer = layer
 
     def activate(self, activations):
-
+        """Propagate activations to the next layer"""
         self.activations = activations
 
+        # Compute the propagation to each node in the next layer
         propagation = np.matmul(self.activations, self.weights)
 
+        # Apply logistic function to each
         propagation = np.apply_along_axis(logistic, 0, propagation)
-
-
         if not isinstance(self.next_layer, OutputLayer):
+
+            # Add the constant 1 node to the propagation
             propagation = np.append(propagation, [1])
         
         self.next_layer.activate(propagation)
 
-
     def compute_deltas(self):
+        """Computes deltas for backpropagation.
+         Assumes that the next layer's deltas are current."""
         self.deltas = []
 
+        # This does the summation computation in one step
         sums = np.matmul(self.weights, self.next_layer.deltas)
         for j, a in enumerate(self.activations[:-1]):
             delta = a*(1-a)*sums[j]
             self.deltas.append(delta)
 
 
-
 class OutputLayer():
+    """Output layer. Doesn't need a lot of functionality except for computing deltas."""
     def __init__(self, size):
         self.size = size
         self.activations = []
@@ -172,7 +189,6 @@ class OutputLayer():
         """Compute deltas for output layer"""
         self.deltas = []
         a = self.activations
-        # print("data:{} activations:{}\ndifference:{}".format(y,a,[y[j]-a[j] for j in range(len(y))]))
         for j in range(len(y)):
             delta_j = a[j] * (1-a[j]) * (y[j] - a[j])
             self.deltas.append(delta_j)
@@ -183,44 +199,39 @@ class OutputLayer():
     def __getitem__(self, ind):
         return self.activations[ind]
 
-
-def dynamic_alpha(count, const):
-    return const / (const + count)
-
-def static_alpha(count, const):
-    return const
-
-def k_groups(data, k):
-    random.shuffle(data)
-    groups = []
-    for i in range(k):
-        groups.append(data[int(i*len(data)/k):int((i+1)*len(data)/k)])
-    return groups
-
-
 class NeuralNetwork():
     def __init__(self, layer_sizes, alpha_fn, alpha_const):
+        """Initializes a neural network given layer sizes and a function and constant used to compute the alpha for each layer"""
+
+
         hidden_layer_sizes = layer_sizes[:-1]
         output_layer_size = layer_sizes[-1]
         self.num_layers = len(layer_sizes)
         self.layers = []
         for i in range(len(layer_sizes) - 1):
             self.layers.append(Layer(size = layer_sizes[i], next_size = layer_sizes[i+1]))
+        
 
         self.input_layer = self.layers[0]
         self.output_layer = OutputLayer(output_layer_size)
         self.layers.append(self.output_layer)
+
+
+        # Alpha_fn is either static_alpha or dynamic_alpha. 
+        # Both take in the epoch number but only dynamic_alpha uses it.
+        # I'm doing it this way so that it is picklable
         self.alpha_fn = alpha_fn
         self.alpha_const = alpha_const
-
 
         for i in range(len(self.layers) - 1):
             self.layers[i].connect(self.layers[i+1])
 
     def predict_class(self, _input):
-
+        """Predict the class of an input"""
         self.eval(_input)
+
         if len(self.output_layer) == 1:
+            # If only 2 classes, just round the output node
             return round(self.output_layer[0])
         else:
             max_index = 0
@@ -235,6 +246,7 @@ class NeuralNetwork():
         return self.alpha_fn(count, self.alpha_const)
 
     def forward_propagate(self, data):
+        """Given a piece of data, adds constant 1 to it and then sends it to the input layer"""
         data = np.asarray(data)
         data = np.append(data, [1])
         self.input_layer.activate(data)
@@ -247,37 +259,35 @@ class NeuralNetwork():
         return self.output_layer.activations
 
     def backpropagate(self, y):
+        """Backpropagates error deltas"""
+
+        # Do this one separately because it requires a parameter
         self.output_layer.compute_deltas(y)
         for layer in self.layers[::-1][1:]:
             layer.compute_deltas()
 
     def update_weights(self, alpha):
+        """Updates weights based on deltas"""
 
         for layer in self.layers[:-1]:
-
             for i, a_i in enumerate(layer.activations):
-
                 for j in range(len(layer.weights[0])):
-
                     delta_j = layer.next_layer.deltas[j]
-                    start = layer.weights[i,j]
                     layer.weights[i,j] += alpha * a_i * delta_j
-                    end = layer.weights[i,j]
-
-
-                    assert abs((end - start)- (alpha * a_i * delta_j)) < .00001
 
     def train(self, data, max_epochs, time = np.inf):
+        """Trains using a dataset"""
         start_time = datetime.datetime.now()
         num_epochs = 0
-        not_happy_yet = True 
+        happy = False
 
-        while not_happy_yet:
-            
+        while not happy:
 
             for x,y in data:
                 self.forward_propagate(x)
                 self.backpropagate(y)
+
+                # I lost hours of my life because this line of code was unindented
                 self.update_weights(alpha = self.get_alpha(num_epochs))
                 
             num_epochs += 1
@@ -286,54 +296,63 @@ class NeuralNetwork():
             if num_epochs > max_epochs or (time != np.inf and time_elapsed > time):
                 break
             print("{0:05d}".format(num_epochs), end = '\r')
-    def print_weights(self):
-        for layer in self.layers[:-1]:
-            print(layer.weights)
 
 def map_to_accuracy(args):
+    """Helper function to allow me to pass a single parameter to map function for mp"""
     grouped_pair, structure, alpha_fn, alpha_const, epochs = args
+    
     nn = NeuralNetwork(structure, alpha_fn, alpha_const)
     training_data, test_data = grouped_pair
     nn.train(training_data, epochs)
+    
     return accuracy(nn, test_data)
 
 def eval_k_groups(structure, k, pairs, alpha_fn, alpha_const, epochs):
-    grouped_pairs = []
+    """Performs k-fold cross-validation"""
+    grouped_params = []
     accs = []
     groups = k_groups(data = pairs, k = k)
     for i in range(len(groups)):
         test_data = groups[i]
+
+        # Flattened list of all data not in the test set
         training_data = [x for group in groups for x in group if group != test_data]
-        grouped_pairs.append(((training_data, test_data), structure, alpha_fn, alpha_const, epochs))
+        grouped_params.append(((training_data, test_data), structure, alpha_fn, alpha_const, epochs))
 
     with Pool(4) as pool:
-        acc_list = pool.map(map_to_accuracy, grouped_pairs)
+        acc_list = pool.map(map_to_accuracy, grouped_params)
 
     avg_accuracy = sum(acc_list)/len(acc_list)
-    # print("avg_accuracy:{}".format(avg_accuracy))
     return avg_accuracy
 
-def run_en_masse():
+def main():
+    """ Train neuralnetwork(s)"""
+
+    # Run parameters --------------------------------------------
     epoch_counts = [50,1000]
 
-    hidden_structs = [[x] for x in range(2,8)]
-    
+    # This only determines the size of the hidden layer(s) since the input and output layers are created based on the data.
+    hidden_structs = [[x] for x in range(2,8)] + [[4,4]]
     static_alpha_consts = [.1,.01]
 
+    # Dynamic alpha: X/(X+count)
     dynamic_alpha_consts = [1000,5000]
+    output_filename = "wineoutput.csv"
+    filenames = ["wine.csv"]
+    num_groups = 10
+    # -----------------------------------------------------------
     
     alpha_fns = ([(static_alpha, const) for const in static_alpha_consts] + 
             [(dynamic_alpha, const) for const in dynamic_alpha_consts])
-    
+
+    # This allows the alpha functions to be paired with a nice string representation for the CSV/spreadsheet
     alpha_displays = [str(alpha) for alpha in static_alpha_consts] + ["{}/{}+count".format(d_a,d_a) for d_a in dynamic_alpha_consts]
 
-    alpha_tuples = [(alpha_fns[i][0], alpha_fns[i][1], alpha_displays[i]) for i in range(len(alpha_displays)) ]
-
-    filenames = ["wine.csv"]
+    alpha_tuples = [(alpha_fns[i][0], alpha_fns[i][1], alpha_displays[i]) for i in range(len(alpha_displays))]    
 
     possible_runs = [(epoch_count, struct, alpha_tuple) for struct in hidden_structs for alpha_tuple in alpha_tuples for epoch_count in epoch_counts]
 
-    output_filename = "wineoutput.csv"
+    
 
     with open(output_filename, "a") as csvfile:
         writer = csv.writer(csvfile, delimiter = ',')
@@ -349,84 +368,17 @@ def run_en_masse():
 
             alpha_fn, alpha_const, alpha_display = alpha_tuple
 
+            # Determines the length of the input and output layers from the data
             input_len = len(pairs[0][0])
             output_len = len(pairs[0][1])
             structure = [input_len] + struct + [output_len]
-             
-            accuracy = eval_k_groups(structure = structure, alpha_fn = alpha_fn, alpha_const = alpha_const, k = 10, pairs = pairs, epochs = epochs)
+            
+            accuracy = eval_k_groups(structure = structure, alpha_fn = alpha_fn, alpha_const = alpha_const,
+                                            k = num_groups, pairs = pairs, epochs = epochs)
+
+            # Reopening the file each time so that if my stupid laptop BSOD's while I'm running tests I don't lose the data (I think).
             with open(output_filename, "a") as csvfile:
                 writer = csv.writer(csvfile, delimiter = ',')
                 writer.writerow([data_filename, structure, alpha_display,epochs, accuracy])
-
-def main():
-    run_en_masse()
-    return
-    epochs = EPOCHS
-    hidden_structure = [7]
-    using_dynamic_alpha = True
-    data_filename = "increment-3-bit.csv" if len(sys.argv) < 2 else sys.argv[1]
-
-    
-
-    header, data = read_data(data_filename, ",")
-    pairs = convert_data_to_pairs(data, header)
-
-
-    input_len = len(pairs[0][0])
-    output_len = len(pairs[0][1])
-    structure = [input_len] + hidden_structure + [output_len]
-
-    if data_filename == "increment-3-bit.csv":
-        training_data = pairs
-        test_data = pairs
-        nn = NeuralNetwork(structure)
-        nn.print_weights()
-        groups = k_groups(pairs, 10)
-
-    else:
-        accuracy = eval_k_groups(structure = structure, k = 10, pairs = pairs)
-        print("\n\nStructure:{}\nOverall accuracy: {}".format(structure, accuracy))
-        return
-
-
-
-
-    # # Note: add 1.0 to the front of each x vector to account for the dummy input
-    # training = [([1.0] + x, y) for (x, y) in pairs]
-
-    # # Check out the data:
-    # for example in training:
-    #     print(example)
-    
-
-    
-    
-
-    nn.train(training_data, dynamic_alpha, max_epochs = epochs)
-    if data_filename != "increment-3-bit.csv":
-        accs = []
-        for i in range(len(groups)):
-            test_data = groups[i]
-            training_data = [x for x in group for group in groups if group is not test_data]
-            accs.append("accuracy", accuracy(nn, test_data))
-        avg_accuracy = sum(accs)/len(accs)
-        print("avg_accuracy:{}".format(avg_accuracy))
-    else:
-
-        for x, y in pairs:
-            output = nn.eval(x)
-            ftd = [str(p)[:5] for p in output]
-            print('input:{} output:{} desired:{}'.format(x, ftd, y))
-    nn.print_weights()
-
-    with open("outfile.txt", "a") as outfile:
-        alpha_string = "{}/{}+epoch_num".format(DYNAMIC_ALPHA_CONSTANT,DYNAMIC_ALPHA_CONSTANT) if using_dynamic_alpha else str(STATIC_ALPHA)
-        outfile.write("Epochs:{}structure:{}alpha:{}".format(epochs, structure, alpha_string))
-
-    ### I expect the running of your program will work something like this;
-    ### this is not mandatory and you could have something else below entirely.
-    # nn = NeuralNetwork([3, 6, 3])
-    # nn.back_propagation_learning(training)
-
 if __name__ == "__main__":
     main()
